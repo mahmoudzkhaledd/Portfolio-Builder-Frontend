1b7c580900a9ca5b03543c3f3440362cdfdb3806
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-12-04 16:54:48 +0200

    front

--- src/Utils/Axios.js
+++ src/Utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3007/';
+axios.defaults.baseURL = 'https://portfolio-builder-backend.vercel.app/';
 //axios.defaults.baseURL = 'http://192.168.1.8:3007/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials= true;
