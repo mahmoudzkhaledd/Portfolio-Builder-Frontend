5bfe5644f7748842746f4105cb1b24246a741042
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-12-06 20:13:35 +0200

    validataaa

--- src/Utils/RouterValidatorClient.jsx
+++ src/Utils/RouterValidatorClient.jsx
@@ -5,27 +5,56 @@ import { useDispatch } from 'react-redux';
 import { useRouter } from 'next/navigation';
 import cookies from 'js-cookie';
 import ModeSwitcher from '@/Components/ModeSwitcher/ModeSwitcher';
+
+
+
+const unProtectedRoutes = [
+    '/login',
+    '/register',
+    '/portfolio/:id',
+];
+const notAlloweRoutes = [
+    '/login',
+    '/register',
+    '/verify-email',
+];
+
+function matchRoute(incomingRoute) {
+    for (const route of unProtectedRoutes) {
+        const regexRoute = new RegExp('^' + route.replace(/:[a-zA-Z0-9]+/g, '([a-zA-Z0-9]+)') + '$');
+        if (regexRoute.test(incomingRoute)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 export default function RouterValidatorClient({ children }) {
     const rout = useRouter();
     const disp = useDispatch();
-
-    useLayoutEffect(() => {
-        const process = async () => {
-            try {
-                const theme = localStorage.getItem('theme') || "light";
-                disp(slice.actions.setTheme(theme));
-                disp(slice.actions.setLoggedIn(true));
-                return; 
-            } catch (ex) {
-                console.log('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
-            }
+    console.log('object');
+    try {
+        const storage = localStorage.getItem('token');
+        const cok = cookies.get('token');
+        if (cok == null || storage == null) {
+            throw "";
         }
-        process();
-    }, []);
+        const theme = localStorage.getItem('theme') || "light";
+        disp(slice.actions.setTheme(theme));
+        disp(slice.actions.setLoggedIn(true));
+
+    } catch (ex) {
+        if(!matchRoute(window.location.pathname)){
+            localStorage.removeItem('token');
+            cookies.remove('token');
+            rout.replace('/login');
+        } 
+    }
     return <div style={{
         width: "100%",
         height: "100%",
-        
+
     }}>
         <div style={{
             zIndex: 1000,
