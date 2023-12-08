105db0173619df5f320bfce7cfafdd65f96ce9d6
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2023-12-08 22:50:44 +0200

    dayyys

--- src/Components/Dashboard/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Pages/Dashboard/Dashboard.jsx
@@ -8,51 +8,12 @@ import { dashContext } from "@/hooks/state/dashboardState";
 import { switchPortfolioState } from '../../../../Controllers/UserProfile/UserProfileCtrl';
 import { Line } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto';
-const data = [
-    {
-        "name": "Page A",
-        "uv": 4000,
-        "pv": 2400,
-        "amt": 2400
-    },
-    {
-        "name": "Page B",
-        "uv": 3000,
-        "pv": 1398,
-        "amt": 2210
-    },
-    {
-        "name": "Page C",
-        "uv": 2000,
-        "pv": 9800,
-        "amt": 2290
-    },
-    {
-        "name": "Page D",
-        "uv": 2780,
-        "pv": 3908,
-        "amt": 2000
-    },
-    {
-        "name": "Page E",
-        "uv": 1890,
-        "pv": 4800,
-        "amt": 2181
-    },
-    {
-        "name": "Page F",
-        "uv": 2390,
-        "pv": 3800,
-        "amt": 2500
-    },
-    {
-        "name": "Page G",
-        "uv": 3490,
-        "pv": 4300,
-        "amt": 2100
-    }
-]
 
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
 export default function DashboardPage() {
     const port = useContext(dashContext);
     const [loading, setLoading] = useState({
@@ -78,7 +39,7 @@ export default function DashboardPage() {
     }
     loading.data.viewsHistory = loading.data.viewsHistory || [];
     const chartData = {
-        labels: loading.data.viewsHistory.map(e => moment(e.date).format("MMM Do YY")),
+        labels: loading.data.viewsHistory.map(e => moment(new Date(e.date).addDays(1)).format("MMM Do YY")),
         datasets: [{
             label: "",
             data: loading.data.viewsHistory.map(e => e.views),
