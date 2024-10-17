97d84a1d8ed4468407d9f4d38f9827797a2fd7a3
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2024-10-17 19:21:37 +0300

    fdfd

--- src/Components/Templates/MainTemplate/Hero/MainHero.jsx
+++ src/Components/Templates/MainTemplate/Hero/MainHero.jsx
@@ -1,34 +1,42 @@
-import style from './style.module.css';
-import Animation from './Animation'
+import style from "./style.module.css";
+import Animation from "./Animation";
 export default function MainHero({ data }) {
-    
-    return (
-        <div className={style.mainContainer} id={data.id}>
-            <div>
-                <div className={style.blur}></div>
-                <div className={style.detailsCont}>
-                    {data.leftImage && <img
-					aspectRation={1}
-                        src={data.leftImage.link}
-                        width={data.leftImage.width}
-                        height={data.leftImage.height}
-                        alt=''
-                        className={style.circleImage}
-                    />}
-                    <p className={style.header}>
-                        {data.headerTitle}
-                    </p>
-                    <br />
-                    <p className={style.subtitle}>
-                        {data.subTitle}
-                    </p>
-                </div>
-            </div>
-            {
-                data.rightImage.laptop ? <Animation className={style.animation} /> :
-                    (data.rightImage && <img  className={data.rightImage.floating ? `${'floatingImage'} ${style.rightImage}` : style.rightImage} src={data.rightImage.link} width={data.rightImage.width} height={data.rightImage.height} />)
-            }
-
+  return (
+    <div className={style.mainContainer} id={data.id}>
+      <div>
+        <div className={style.blur}></div>
+        <div className={style.detailsCont}>
+          {data.leftImage && data.leftImage.link != "" && (
+            <img
+              aspectRation={1}
+              src={data.leftImage.link}
+              width={data.leftImage.width}
+              height={data.leftImage.height}
+              alt=""
+              className={style.circleImage}
+            />
+          )}
+          <p className={style.header}>{data.headerTitle}</p>
+          <br />
+          <p className={style.subtitle}>{data.subTitle}</p>
         </div>
-    )
+      </div>
+      {data.rightImage.laptop ? (
+        <Animation className={style.animation} />
+      ) : (
+        data.rightImage && (
+          <img
+            className={
+              data.rightImage.floating
+                ? `${"floatingImage"} ${style.rightImage}`
+                : style.rightImage
+            }
+            src={data.rightImage.link}
+            width={data.rightImage.width}
+            height={data.rightImage.height}
+          />
+        )
+      )}
+    </div>
+  );
 }
