ca17bf4526b10b11752993b01e0a90203f37bce6
mahmoudzkhaledd <mahmoudnaggar2002@gmail.com>
2024-10-27 20:46:30 +0300

    dfdf

--- src/Components/Templates/MainTemplate/Projects/ProjectCard.jsx
+++ src/Components/Templates/MainTemplate/Projects/ProjectCard.jsx
@@ -1,19 +1,20 @@
-import Image from 'next/image';
-import style from './style.module.css';
-import Link from 'next/link';
+import Image from "next/image";
+import style from "./style.module.css";
+import Link from "next/link";
 export default function ProjectCard({ title, description, image, link }) {
-    return (
-        <Link href={link} target="_blank" rel="noopener noreferrer">
-            <div className={style.projectCard}>
-                <img
-                    src={image}
-                    className={style.cardImage}
-                />
-                <div className={style.cardTitleContainer}>
-                    <h1 className={style.cardTitle}>{title}</h1>
-                    <p className={style.cardSubTitle}>{description}</p>
-                </div>
-            </div>
-        </Link>
-    )
+  return (
+    <Link href={link} target="_blank" rel="noopener noreferrer">
+      <div className={style.projectCard}>
+        <img src={image} className={style.cardImage} />
+        <div className={style.cardTitleContainer}>
+          <h1 className={style.cardTitle}>{title}</h1>
+          <div
+            className={style.cardSubTitle}
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+          {/* <p className={style.cardSubTitle}>{description}</p> */}
+        </div>
+      </div>
+    </Link>
+  );
 }
